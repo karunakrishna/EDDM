ad608acbadacb01cb437f8b12af6afe417b9a5ea
karunakrishna <karuna.krishna@exterro.com>
2018-12-19 18:39:27 +0530

    newcommitKaruna

--- Sample_for_git/src/Test_Demo01.java
+++ Sample_for_git/src/Test_Demo01.java
@@ -4,8 +4,8 @@ public class Test_Demo01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String [] name = {"Apple","moon","sun","Mango"};
-		for(int i = 0 ; i< name.length;i++){
-			System.out.println(name[i]);
+		for(String dest : name){
+			System.out.println(dest);
 		}
 
 	}
