3b98049ff0b188a2f8b045074844261fedf0ced5
karunakrishna <karuna.krishna@exterro.com>
2018-12-19 18:43:00 +0530

    new commit by karu

--- Sample_for_git/src/Test_Demo01.java
+++ Sample_for_git/src/Test_Demo01.java
@@ -4,6 +4,7 @@ public class Test_Demo01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String [] name = {"Apple","moon","sun","Mango","yellow","Green","pink"};
+		System.out.println("pls commit my new code!!");
 		for(int i = 0 ; i< name.length;i++){
 			System.out.println(name[i]);
 			System.out.println("how are you Exterro!!!");
