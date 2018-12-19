151fa9a168f9fa5ed35c2d088046af4ec7050e21
karunakrishna <karuna.krishna@exterro.com>
2018-12-19 18:41:15 +0530

    commitbykaru

--- Sample_for_git/src/Test_Demo01.java
+++ Sample_for_git/src/Test_Demo01.java
@@ -3,9 +3,10 @@ public class Test_Demo01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String [] name = {"Apple","moon","sun","Mango"};
+		String [] name = {"Apple","moon","sun","Mango","yellow","Green","pink"};
 		for(int i = 0 ; i< name.length;i++){
 			System.out.println(name[i]);
+			System.out.println("how are you Exterro!!!");
 		}
 
 	}
