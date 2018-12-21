65955ccd1344ce91af38bf5ff490f2ef58dbfb8d
karunakrishna <karuna.krishna@exterro.com>
2018-12-21 15:21:52 +0530

    Employee01 commit

---
+++ Demo06/src/Employee.java
@@ -0,0 +1,13 @@
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] emp= {"Jack","Jill","King"};
+		for(int i=0;i<=10;i++){
+			System.out.println(emp[i]);
+		}
+
+	}
+
+}
