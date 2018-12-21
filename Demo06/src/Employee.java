1c08dc2408ae8f75297d0e7d57671aaabc4e3695
karunakrishna <karuna.krishna@exterro.com>
2018-12-21 15:30:20 +0530

    done by employee02

--- Demo06/src/Employee.java
+++ Demo06/src/Employee.java
@@ -4,9 +4,11 @@ public class Employee {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String [] emp= {"Jack","Jill","King"};
-		for(int i=0;i<=10;i++){
-			System.out.println(emp[i]);
+		for(String ax : emp){
+			System.out.println(ax);
 			System.out.println("commit is done!!!");
+			System.out.println("new commit is done!!!");
+			
 		}
 
 	}
